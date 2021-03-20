9dc6c6c6ed8418ac8aae12e1f181e83c8b703c8f
Stephen Clark <sdclarkie@gmail.com>
2021-03-20 11:32:33 -0400

    challenges

--- Interview Practice/2-1-2021/index.ts
+++ Interview Practice/2-1-2021/index.ts
@@ -1,30 +1,30 @@
 /**
  * DO NOT CHANGE CODE BETWEEN COMMENT BLOCKS
  */
-///////////////////////////////////////////////////////////////////
-const [A, B, C] = ["A", "B", "C"];
-function getA() {
-  return A;
-}
-function getB(callback) {
-  setTimeout(() => {
-    callback(B);
-  }, 10);
-}
-function getC() {
-  return Promise.resolve().then(() => C);
-}
-///////////////////////////////////////////////////////////////////
-// IMPLEMENT THIS FUNCITON
-function getABC() {
-  const obj = {};
-  const cb = (b) => (obj[b] = b);
-  getB(cb);
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      res(getC().then((c) => [getA(), obj.B, c]));
-    }, 10);
-  });
-}
-getABC().then((arr) => console.log(arr, ["A", "B", "C"]));
+// ///////////////////////////////////////////////////////////////////
+// const [A, B, C] = ['A', 'B', 'C'];
+// function getA() {
+//   return A;
+// }
+// function getB(callback) {
+//   setTimeout(() => {
+//     callback(B);
+//   }, 10);
+// }
+// function getC() {
+//   return Promise.resolve().then(() => C);
+// }
+// ///////////////////////////////////////////////////////////////////
+// // IMPLEMENT THIS FUNCITON
+// function getABC() {
+//   const obj = {};
+//   const cb = (b) => (obj[b] = b);
+//   getB(cb);
+//   return new Promise((res, rej) => {
+//     setTimeout(() => {
+//       res(getC().then((c) => [getA(), obj.B, c]));
+//     }, 10);
+//   });
+// }
+// getABC().then((arr) => console.log(arr, ['A', 'B', 'C']));
 
